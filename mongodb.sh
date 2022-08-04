6e2626f6d639df4b2ebb90516f2649a16662d9da
rahulvamsi <p.rahulvamsi@gmail.com>
2022-08-04 19:47:31 +0530

    line added

--- mongodb.sh
+++ mongodb.sh
@@ -1,3 +1,4 @@
+set -e
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
 yum install -y mongodb-org
 systemctl enable mongod
@@ -6,7 +7,7 @@ systemctl restart mongod
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
 
 cd /tmp
-unzip -o mongodb.zip
+unzip  mongodb.zip
 cd mongodb-main
 mongo < catalogue.js
 mongo < users.js
