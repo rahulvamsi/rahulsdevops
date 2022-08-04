870ee0a83350151a4598fe1b3b3a67a23c01561a
rahulvamsi <p.rahulvamsi@gmail.com>
2022-08-04 20:16:35 +0530

    robshop2 added

--- catalogue.sh
+++ catalogue.sh
@@ -1,7 +1,7 @@
 set -e
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
-userdel -f roboshop
+userdel -f -r roboshop
 useradd roboshop
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
 cd /home/roboshop
