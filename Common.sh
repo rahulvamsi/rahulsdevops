69bf33aa07e9df170f605bb97f392f601550f3b2
rahulvamsi <p.rahulvamsi@gmail.com>
2022-08-08 19:00:33 +0530

    reduce line

--- Common.sh
+++ Common.sh
@@ -29,7 +29,8 @@ echo "downloading zip file "
 curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>/tmp/${COMPONENT}.log
 StatusCheck
 echo "changing direct"
-cd /home/roboshop &>>/tmp/${COMPONENT}.log &&  rm -rf ${COMPONENT} &>>/tmp/${COMPONENT}.log
+cd /home/roboshop &>>/tmp/${COMPONENT}.log
+rm -rf ${COMPONENT} &>>/tmp/${COMPONENT}.log
 StatusCheck
 echo "unzipping"
  unzip -o /tmp/${COMPONENT}.zip
