07e8be67c4460b663f29822e5c5de7c17e070b05
rahulvamsi <p.rahulvamsi@gmail.com>
2022-08-08 17:22:48 +0530

    checking if

--- cart.sh
+++ cart.sh
@@ -1,13 +1,14 @@
-set -e
+#set -e
 
 echo "downloading repo"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/cart.log
 echo  $?
-if [$? -eq 0 ]
+if [ $? -eq 0 ];
 then
   echo  " sucess"
 else
   echo "failed"
+fi
 echo "installing node"
 yum install nodejs -y &>>/tmp/cart.log
 echo  $?
