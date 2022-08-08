999d964939e60e4bb8f1ed162a0efa13596c0191
rahulvamsi <p.rahulvamsi@gmail.com>
2022-08-08 19:11:51 +0530

    reduce line

--- cart.sh
+++ cart.sh
@@ -4,7 +4,7 @@ source Common.sh
 NodeJS
 
 echo "unzipping"
-unzip -o /tmp/cart.zip
+unzip -o /tmp/cart.zip &>>/tmp/cart.log
 StatusCheck
 echo " moving and changing"
 mv cart-main cart &>>/tmp/cart.log && cd cart &>>/tmp/cart.log
@@ -13,7 +13,7 @@ echo "installing node "
 npm install &>>/tmp/cart.log
 StatusCheck
 echo "moving systemd to cart"
-mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>>/tmp/cart.log
+mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
 StatusCheck
 echo " running systemctl commands"
 systemctl daemon-reload &>>/tmp/cart.log &&  systemctl start cart &>>/tmp/cart.log  && systemctl enable cart &>>/tmp/cart.log
