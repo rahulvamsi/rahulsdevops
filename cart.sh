518f2b8529cd4c6bd95a4497653eb10b392f9332
rahulvamsi <p.rahulvamsi@gmail.com>
2022-08-08 17:24:13 +0530

    checking if

--- cart.sh
+++ cart.sh
@@ -2,7 +2,6 @@
 
 echo "downloading repo"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/cart.log
-echo  $?
 if [ $? -eq 0 ];
 then
   echo  " sucess"
@@ -11,36 +10,91 @@ else
 fi
 echo "installing node"
 yum install nodejs -y &>>/tmp/cart.log
-echo  $?
+if [ $? -eq 0 ];
+then
+  echo  " sucess"
+else
+  echo "failed"
+fi
 echo "addind user"
 useradd roboshop &>>/tmp/cart.log
-echo  $?
+if [ $? -eq 0 ];
+then
+  echo  " sucess"
+else
+  echo "failed"
+fi
 echo "downloading zip file "
 curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" &>>/tmp/cart.log
-echo  $?
+if [ $? -eq 0 ];
+then
+  echo  " sucess"
+else
+  echo "failed"
+fi
 echo "changing direct"
 cd /home/roboshop &>>/tmp/cart.log
-echo  $?
+if [ $? -eq 0 ];
+then
+  echo  " sucess"
+else
+  echo "failed"
+fi
 echo " removing cart user"
 rm -rf cart &>>/tmp/cart.log
-echo  $?
+if [ $? -eq 0 ];
+then
+  echo  " sucess"
+else
+  echo "failed"
+fi
 echo "unzipping"
 unzip -o /tmp/cart.zip &>>/tmp/cart.log
-echo  $?
+if [ $? -eq 0 ];
+then
+  echo  " sucess"
+else
+  echo "failed"
+fi
 echo "moving"
 mv cart-main cart &>>/tmp/cart.log
-echo  $?
+if [ $? -eq 0 ];
+then
+  echo  " sucess"
+else
+  echo "failed"
+fi
 echo "changing dir"
 cd cart &>>/tmp/cart.log
-echo  $?
+if [ $? -eq 0 ];
+then
+  echo  " sucess"
+else
+  echo "failed"
+fi
 echo "installing node "
 npm install &>>/tmp/cart.log
-echo  $?
+if [ $? -eq 0 ];
+then
+  echo  " sucess"
+else
+  echo "failed"
+fi
 echo "moving systemd to cart"
 mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>>/tmp/cart.log
-echo  $?
+if [ $? -eq 0 ];
+then
+  echo  " sucess"
+else
+  echo "failed"
+fi
 echo " running systemctl commands"
 systemctl daemon-reload &>>/tmp/cart.log
 systemctl start cart &>>/tmp/cart.log
 systemctl enable cart &>>/tmp/cart.log
-echo  $?
+if [ $? -eq 0 ];
+then
+  echo  " sucess"
+else
+  echo "failed"
+fi
