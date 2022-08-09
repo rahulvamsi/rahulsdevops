db2a8d5e80b30694ae60e1e80efd14ed0d9c6d15
rahulvamsi <p.rahulvamsi@gmail.com>
2022-08-09 21:04:55 +0530

    testing repo

--- mongodb.sh
+++ mongodb.sh
@@ -4,6 +4,11 @@ source Common.sh
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>${LOG} && yum install -y mongodb-org &>>${LOG}
 echo configuring systemd
 systemctl enable mongod && systemctl start mongod && systemctl restart mongod
+echo changing ip to 0
+sed -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+StatusCheck
+
+
 StatusCheck
 DOWNLOAD
 echo INSTALLING NODEJS DEPENDENCIES
