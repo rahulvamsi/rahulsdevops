1e57f959327a49c1723a84d0ab7995efac010b72
rahulvamsi <p.rahulvamsi@gmail.com>
2022-08-09 19:57:28 +0530

    testing repo

--- dispatch.sh
+++ dispatch.sh
@@ -25,12 +25,9 @@ echo INSTALLING GOLANG
 yum install golang -y &>>${LOG}
 StatusCheck
 APP_USER_SETUP
-cd /home/roboshop
-rm -rf dispatch
+cd /home/roboshop && rm -rf dispatch
 curl -L -s -o /tmp/dispatch.zip https://github.com/roboshop-devops-project/dispatch/archive/refs/heads/main.zip
-unzip -o /tmp/dispatch.zip
-mv dispatch-main dispatch
-cd dispatch
+unzip -o /tmp/dispatch.zip && mv dispatch-main dispatch && cd dispatch
 
 echo RUNNING GO SERVICE
 go mod init dispatch &>>${LOG} && go get &>>${LOG} && go build &>>${LOG}
