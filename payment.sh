30b8859901c90834985dc248a64b7a53048eddec
rahulvamsi <p.rahulvamsi@gmail.com>
2022-08-09 21:34:52 +0530

    testing repo

--- payment.sh
+++ payment.sh
@@ -1,15 +1,4 @@
-set -e
-yum install python36 gcc python3-devel -y
-userdel -r -f roboshop
-useradd roboshop
-cd /home/roboshop
-rm -rf payment
-curl -L -s -o /tmp/payment.zip "https://github.com/roboshop-devops-project/payment/archive/main.zip"
-unzip -o /tmp/payment.zip
-mv payment-main payment
-cd /home/roboshop/payment
-pip3 install -r requirements.txt
-mv /home/roboshop/payment/systemd.service /etc/systemd/system/payment.service
-systemctl daemon-reload
-systemctl enable payment
-systemctl start payment
+#!/usr/bin/env bash
+COMPONENT=payment
+source Common.sh
+PYTHON
