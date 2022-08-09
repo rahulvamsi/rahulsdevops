2c68872a56f4c9595f207cef800806d06f40fdca
rahulvamsi <p.rahulvamsi@gmail.com>
2022-08-09 19:31:35 +0530

    testing repo

--- mongodb.sh
+++ mongodb.sh
@@ -2,10 +2,21 @@
 COMPONENT=mongodb
 source Common.sh
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>${LOG} && yum install -y mongodb-org &>>${LOG}
-SYSTEMD
+echo configuring systemd
+systemctl enable mongod
+systemctl start mongod
+systemctl restart mongod
 DOWNLOAD
 echo INSTALLING NODEJS DEPENDENCIES
 cd /tmp &>>${LOG} && rm -rf mongodb &>>${LOG} && unzip -o mongodb.zip &>>${LOG} && cd mongodb-main &>>${LOG}
 echo GETTING  FILE DATA IN MONGODB
 mongo < catalogue.js &>>${LOG} &&mongo < users.js &>>${LOG}
+
+
+
+
+
+
+
+
 
