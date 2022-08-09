ae97f6f5ce03fd957221192e1a1b0d5ebbf6caf6
rahulvamsi <p.rahulvamsi@gmail.com>
2022-08-09 19:40:02 +0530

    testing repo

--- dispatch.sh
+++ dispatch.sh
@@ -1,15 +1,14 @@
-set -e
-yum install golang -y
-useradd roboshop
-rm -rf dispatch
-curl -L -s -o /tmp/dispatch.zip https://github.com/roboshop-devops-project/dispatch/archive/refs/heads/main.zip
-unzip -o /tmp/dispatch.zip
-mv dispatch-main dispatch
-cd dispatch
-go mod init dispatch
-go get
-go build
-mv /home/roboshop/dispatch/systemd.service /etc/systemd/system/dispatch.service
-systemctl daemon-reload
-systemctl enable dispatch
-systemctl start dispatch
+#!/usr/bin/env bash
+COMPONENT=dispatch
+source Common.sh
+
+echo INSTALLING GOLANG
+yum install golang -y &>>${LOG}
+StatusCheck
+APP_USER_SETUP
+APP_CLEAN
+
+echo RUNNING GO SERVICE
+go mod init dispatch &>>${LOG} && go get &>>${LOG} && go build &>>${LOG}
+StatusCheck
+SYSTEMD
--- redis.sh
+++ redis.sh
@@ -1,5 +1,4 @@
-set -e
+#!/usr/bin/env bash
 curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
 yum install redis-6.2.7 -y
-systemctl enable redis
-systemctl start redis
+systemctl enable redis && systemctl start redis
