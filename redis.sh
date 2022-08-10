f866355af8d239ccae718086513cf85e2a5e962e
rahulvamsi <p.rahulvamsi@gmail.com>
2022-08-10 21:33:22 +0530

    testong

--- redis.sh
+++ redis.sh
@@ -8,7 +8,7 @@ yum install redis -y &>>${LOG}
 StatusCheck
 
 echo changing ip to 0
-sed -i  's/127.0.0.1/0.0.0.0/g' /etc/redis.conf /etc/redis/redis.conf &>>${LOG}
+sed -i  's/127.0.0.1/0.0.0.0/g' /etc/redis.conf &>>${LOG}
 StatusCheck
 echo CONFIGURING SYSTEMD
 systemctl enable redis && systemctl start redis &>>${LOG}
