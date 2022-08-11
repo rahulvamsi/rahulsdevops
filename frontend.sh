f028595073acf84c20f57be9ef631a15a5f2d706
rahulvamsi <p.rahulvamsi@gmail.com>
2022-08-11 20:55:21 +0530

    testing server

--- frontend.sh
+++ frontend.sh
@@ -21,6 +21,10 @@ echo changing localhost to
 sed -i -e '14 s/localhost/catalogue_dev/' -e  '16 s/localhost/user_dev/' -e  '18 s/localhost/cart_dev/' -e  '20 s/localhost/shipping_dev/' -e  '22 s/localhost/payment_dev/' /etc/nginx/default.d/roboshop.conf
 StatusCheck
 
+echo Starting ${COMPONENT} Service
+systemctl start ${COMPONENT} &>>${LOG} && systemctl enable ${COMPONENT} &>>${LOG}
+StatusCheck
+
 echo restarting service nginx
 systemctl restart nginx
 StatusCheck
