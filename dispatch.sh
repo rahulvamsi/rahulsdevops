cd36580b1956e4be32600ad19fb9f99c9978e717
rahulvamsi <p.rahulvamsi@gmail.com>
2022-08-11 19:41:43 +0530

    testing server

--- dispatch.sh
+++ dispatch.sh
@@ -6,20 +6,21 @@ echo INSTALLING GOLANG
 yum install golang -y &>>${LOG}
 StatusCheck
 APP_USER_SETUP
+DOWNLOAD
 
-#APP_CLEAN
+APP_CLEAN
 
-echo Cleaning old application content
-cd /home/roboshop
-
-rm -rf dispatch
-StatusCheck
-
-echo Extract Application Archive
-unzip -o /tmp/dispatch.zip
-mv dispatch-main ${COMPONENT}
-cd dispatch
-StatusCheck
+#echo Cleaning old application content
+#cd /home/roboshop
+#
+#rm -rf dispatch
+#StatusCheck
+#
+#echo Extract Application Archive
+#unzip -o /tmp/dispatch.zip
+#mv dispatch-main ${COMPONENT}
+#cd dispatch
+#StatusCheck
 
 echo RUNNING GO SERVICE
 go mod init dispatch &>>${LOG} && go get &>>${LOG} && go build &>>${LOG}
