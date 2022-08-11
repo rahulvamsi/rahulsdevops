eaec1890d5915af0bb498b897d98085fed33b07d
rahulvamsi <p.rahulvamsi@gmail.com>
2022-08-11 20:58:35 +0530

    testing server

--- frontend.sh
+++ frontend.sh
@@ -3,10 +3,7 @@ source Common.sh
 echo INSTALLING NGINX SERVICE
 yum install nginx -y &>>${LOG}
 StatusCheck
-echo STARTING NGINX WITH SYSTEMCTL
-systemctl enable nginx &>>${LOG}
-systemctl start nginx &>>${LOG}
-StatusCheck
+
 #curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 DOWNLOAD
 echo CHANGING AND REMOVING
@@ -21,8 +18,10 @@ echo changing localhost to
 sed -i -e '14 s/localhost/catalogue_dev/' -e  '16 s/localhost/user_dev/' -e  '18 s/localhost/cart_dev/' -e  '20 s/localhost/shipping_dev/' -e  '22 s/localhost/payment_dev/' /etc/nginx/default.d/roboshop.conf
 StatusCheck
 
-echo Starting ${COMPONENT} Service
-systemctl start ${COMPONENT} &>>${LOG} && systemctl enable ${COMPONENT} &>>${LOG}
+
+echo STARTING NGINX WITH SYSTEMCTL
+systemctl enable nginx &>>${LOG}
+systemctl start nginx &>>${LOG}
 StatusCheck
 
 echo restarting service nginx
