bcf4cd9fbea0efb2461a5221bf138472cc33ae25
rahulvamsi <p.rahulvamsi@gmail.com>
2022-08-12 16:16:47 +0530

    checking redis server

--- mongodb.sh
+++ mongodb.sh
@@ -1,32 +1,31 @@
-#!/usr/bin/env bash
 COMPONENT=mongodb
 source Common.sh
 
 echo Setup YUM repo
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>${LOG}
 StatusCheck
-echo INSTALL MONGODB
+
+echo Install MongoDB
 yum install -y mongodb-org  &>>${LOG}
+StatusCheck
 
-echo configuring systemd
-systemctl enable mongod && systemctl start mongod && systemctl restart mongod
+echo Update MongoDB Listen Address
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${LOG}
 StatusCheck
-echo changing ip to 0
-sed -i  's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${LOG}
+
+echo Start MongoDB Service
+systemctl enable mongod &>>${LOG} && systemctl restart mongod &>>${LOG}
 StatusCheck
+
 DOWNLOAD
-echo INSTALLING NODEJS DEPENDENCIES
-cd /tmp &>>${LOG} && rm -rf mongodb &>>${LOG} && unzip -o mongodb.zip &>>${LOG} && cd mongodb-main &>>${LOG}
-StatusCheck
-echo GETTING  FILE DATA IN MONGODB
-mongo < catalogue.js &>>${LOG} &&mongo < users.js &>>${LOG}
+
+echo "Extract Schema Files"
+cd /tmp && unzip -o mongodb.zip &>>${LOG}
 StatusCheck
 
-
-
-
-
-
-
-
-
+echo Load Schema
+cd mongodb-main
+for schema in catalogue.js users.js ; do
+  mongo <  $schema
+done
+StatusCheck
