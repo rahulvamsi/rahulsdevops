9de49a0e305a580e11ef657d5216174b81fac7cf
rahulvamsi <p.rahulvamsi@gmail.com>
2022-08-12 17:37:16 +0530

    checking redis server

--- dispatch.sh
+++ dispatch.sh
@@ -17,5 +17,11 @@ echo CHANGING RABBITMQ-IP
 sed -i 's/RABBITMQ-IP/rabbitmq_dev.roboshop.internal/' /etc/systemd/system/dispatch.service &>>${LOG} && systemctl daemon-reload &>>${LOG}
 StatusCheck
 
-SYSTEMD
+echo Configuring ${COMPONENT} SystemD Service
+mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>>${LOG} && systemctl daemon-reload &>>${LOG}
+StatusCheck
+
+echo Starting ${COMPONENT} Service
+systemctl start ${COMPONENT} &>>${LOG} && systemctl enable ${COMPONENT} &>>${LOG}
+StatusCheck
 
